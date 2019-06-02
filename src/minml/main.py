5f0c130078b59ca9afb8b0d06049060af456a9ea
Tim Hannifan <hannifan@gmail.com>
2019-06-02 17:39:30 -0500

    adding base config and example data

--- src/minml/main.py
+++ src/minml/main.py
@@ -1,16 +1,28 @@
 import argparse
+import yaml
+import click
 
 class Minml():
-    def __init__(self, in_path, out_path, test, verbose):
-        pass
+    def __init__(self, config, in_path, out_path, test, verbose):
+        self.config = config
+        self.in_path = in_path
+        self.out_path = out_path
+        self.test = test
+        self.verbose = verbose
 
-    def run(self, config_path):
-        pass
+    def run(self):
+        click.echo(f"Running Minml with config: {self.config}")
+        with open(self.config) as f:
+            loaded_config = yaml.load(f)
+
+        print(loaded_config["grid_config"])
 
 if __name__ == "__main__":
 
     desc = ("Main method for running Minml pipeline")
     parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('--config', dest='config', type=str,
+                        help=('Model config path'))
     parser.add_argument('--in_path', dest='in_path', type=str,
                         help=('Input data file location'))
     parser.add_argument('--out_path', dest='out_path', type=str, default='',
@@ -20,12 +32,12 @@ if __name__ == "__main__":
     parser.add_argument('--verbose', dest='verbose', type=bool, default=0,
                         help=("Select verbose logging option."))
     args = parser.parse_args()
-    args_dict = {
+    args_dict = {'config': str(args.config),
                  'in_path': str(args.in_path),
                  'out_path': str(args.out_path),
                  'test': bool(args.test),
                  'verbose': bool(args.verbose)}
 
-    # print(**args_dict)
     m = Minml(**args_dict)
+    m.run()
 
