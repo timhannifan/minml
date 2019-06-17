260490ead45b7f3fb7f31fdb1e318c4f47088a3e
Tim Hannifan <hannifan@gmail.com>
2019-06-17 14:29:01 -0500

    building out pct and top_n metrics

--- src/minml/components/experiment/experiment.py
+++ src/minml/components/experiment/experiment.py
@@ -10,7 +10,12 @@ from components.model.fitter import ModelFitter
 
 from sklearn.model_selection import ParameterGrid
 
+from sklearn.metrics import (accuracy_score, precision_score, recall_score,
+                             f1_score, roc_auc_score)
 
+import warnings
+from sklearn.exceptions import DataConversionWarning
+warnings.filterwarnings(action='ignore', category=DataConversionWarning)
 
 
 class Experiment():
@@ -35,6 +40,40 @@ class Experiment():
                                     quotechar='"', quoting=csv.QUOTE_MINIMAL)
             fmanager.writerow(row)
 
+    def evaluate(self, trained_mode, test_x, test_y):
+        score_config = self.config['scoring']
+
+        testing_metric_list = score_config['testing_metric_groups']
+
+        for metric_dict in testing_metric_list:
+            # precision@, recall@
+            metrics = metric_dict['metrics']
+
+            if 'thresholds' in metric_dict:
+                thresh = metric_dict['thresholds']
+
+                if 'percentiles' in thresh:
+                    for p in thresh['percentiles']:
+                        for m in metrics:
+                            # print(m,p)
+                            self.get_pct_metrics(m, p)
+                if 'top_n' in thresh:
+                    for n in thresh['top_n']:
+                        for m in metrics:
+                            # print(m,n)
+                            self.get_top_n_metrics(m, n)
+
+
+            else:
+                print('no thresholds', metric)
+                pass
+
+
+    def get_pct_metrics(self, metric, pct):
+        print('getting pct metrics', metric, pct)
+    def get_top_n_metrics(self, metric, n):
+        print('getting top n metrics', metric, n)
+
 
     def run(self):
         model_config = self.config['model_config']
@@ -53,16 +92,16 @@ class Experiment():
 
             # Iterate through config models
             for sk_model, param_dict in model_config.items():
-                click.echo(
-                    "\nStarting model: %s on end %s with" % (sk_model, tr_e))
+                # click.echo(
+                    # "\nStarting model: %s on end %s with" % (sk_model, tr_e))
                 param_combinations = list(ParameterGrid(param_dict))
 
                 # For this model, iterate through parameter combinations
                 for params in param_combinations:
                     # pass
                     clf = self.fitter.train(sk_model, params, rich_train_x, train_y)
-                    print(params)
-                    print("model score: %.3f" % clf.score(rich_test_x, test_y))
+
+                    self.evaluate(clf, rich_test_x, test_y)
 
         click.echo(f"Experiment finished")
 
