8cb6b0346f5cd19f3fd413c6875a4677afc945dd
Tim Hannifan <hannifan@gmail.com>
2019-06-17 22:01:24 -0500

    docstring time

--- src/minml/components/experiment/time_utils.py
+++ src/minml/components/experiment/time_utils.py
@@ -2,6 +2,14 @@ from datetime import date, datetime, timedelta
 from dateutil.relativedelta import relativedelta
 
 def get_date_splits(config):
+    """
+    Produces a list of date splits for an experiment using the exp config.
+    Inputs:
+        - config (dict): temporal_config section of config.yaml
+    Returns:
+        (list): Tuples of each split (train_st, train_end, test_st, test_end)
+    """
+
     # start time of our data (yyyy-mm-dd)
     start_time = config['start_time']
     #last date of data including labels and outcomes that we have (yyyy-mm-dd)
@@ -19,6 +27,7 @@ def get_date_splits(config):
     end_time_date = datetime.strptime(end_time, '%Y-%m-%d')
     train_start_time = start_time_date
     res = []
+
     for prediction_window in prediction_windows:
         test_end_time = end_time_date
         while (test_end_time >= start_time_date + 2 * relativedelta(months=+prediction_window)):
