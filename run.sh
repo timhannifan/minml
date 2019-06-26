71d61fb97e25765d6320b95870f6b98b71c55509
Tim Hannifan <hannifan@gmail.com>
2019-06-26 11:03:08 -0400

    updating run.sh

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ source env/bin/activate
 pip install -r requirements.txt
 
 cd src/minml
-python3 main.py --config "../../examples/donors/config/config.yaml" --db "../../examples/donors/config/config_db.yaml"
+python3 main.py --config "../../examples/donors/config/config_test_env.yaml" --db "../../examples/donors/config/config_db.yaml"
 
 deactivate
